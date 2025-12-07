f8ac4f51197df002b64b5dbcfd3e4e36adbc26cf
jwsvengit7 <chiorlujack@gmail.com>
2025-12-07 18:18:53 +0100

    Update

--- src/pages/admin/AdminDashBoard.jsx
+++ src/pages/admin/AdminDashBoard.jsx
@@ -1145,7 +1145,7 @@ const confirmDelete = async () => {
           const errorText = await response.text();
           throw new Error(`Update failed: ${response.status} - ${errorText}`);
         }
-        alert(JSON.stringify(response.body))
+        alert(response.body.status)
            setOrders(
       orders.map((order) =>
         order.id === id ? { ...order, status: "approved" } : order
