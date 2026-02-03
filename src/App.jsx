c8156e32156209598541a01e5c6a677548c6056e
Temple Jack Chiorlu <chiorlujack@gmail.com>
2026-02-03 19:33:30 +0100

    all

--- src/App.jsx
+++ src/App.jsx
@@ -135,10 +135,7 @@ const App = () => {
 
   return (
     <>
-    {/* <center>
-      <h1 style={{fontSize:"50px",fontWeight:"bold"}}>The site subscription is due we cannot proceed until it is renue</h1>
-      </center></> */}
-    
+
     <AuthProvider>
       <ReviewsProvider>
         <ToastProvider>
@@ -258,7 +255,7 @@ const App = () => {
         </ToastProvider>
       </ReviewsProvider>
     </AuthProvider>
-
+</>
    
   );
 };
